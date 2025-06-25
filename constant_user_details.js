47034c448b5d947b8555264aa292f5c549bf072b
Abdul Moiz <amoez897@gmail.com>
2025-06-25 12:50:42 +0500

    added dashboards

---
+++ constant_user_details.js
@@ -0,0 +1,9 @@
+const constantUsers = {
+  "soofia": "beaconhouse123",
+  "bushra": "beaconhouse123",
+  "sania": "beaconhouse123",
+  "asma": "beaconhouse123",
+  "shamsa": "beaconhouse123",
+  "shahzadi": "beaconhouse123",
+  "amna": "beaconhouse123"
+};
